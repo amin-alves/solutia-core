47007721dd4eaae1451f1dc49d0f388978dc93e3
Amin Alves <amin.alves.jr@gmail.com>
2026-02-20 20:35:42 -0400

    Initialize Supabase client with URL and anon key

---
+++ frontend/js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://fzxhhmklosdowjaztjuy.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6eGhobWtsb3Nkb3dqYXp0anV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDI0MzIsImV4cCI6MjA4NzE3ODQzMn0.pInQANuUh-G4PKEwLRTqmyYZHf0NTqp4SQPJuykwQeU";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
